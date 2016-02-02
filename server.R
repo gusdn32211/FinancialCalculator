1256a14984ca605d6a4c7d43b18e03e2a19b11c9
unknown <c.lam@dev-fw-cl.dvl.financialcad.com>
2016-02-02 14:02:24 -0800

    included basic calculations functionality

--- server.R
+++ server.R
@@ -4,137 +4,137 @@ library(shinydashboard)
 
 shinyServer(function(input, output) {
   
+  #Payment period
+  period <- reactive({switch(input$TimeLength,
+                   "Annually" = 1,
+                   "Semi-Annual" = 2,
+                   "Quarterly" = 4,
+                   "Monthly" = 12)
+  })
+  #Interest Rate
+  interestRate <- reactive({
+    (as.numeric(input$InterestRate / 100) / period())
+  })
+  
+  # Formula for payment
+  payment <- reactive({(as.numeric(input$Loan) * interestRate()) / (1 - (1 + interestRate()) ^ (-period() * input$Years))
+  })
+  
+  # Timeframe (years * period)
+  timeframe <- reactive({input$Years * period()})
+  
+  # Initializing list 
+  Year <- reactive({ c(1:timeframe())})
+
+  # Initializing list sizes
+  # Balance <- reactive({c(as.numeric(input$Loan), 2:timeframe())})
+  # Interest <- reactive({c(interestRate()*Balance()[1], 2:timeframe())})
+  # principal_repayment <- reactive({c(payment() - Interest()[1], 2:timeframe())})
+  # ending_balance <- reactive({c(Balance()[1] - principal_repayment()[1], 2:timeframe())})
+  # cumulative_interest <- reactive({c(Interest()[1], 2:timeframe())})
+  # 
+  
+  # 
+  # f <- reactive({
+  #   Balance <- c(1:timeframe())
+  #   ending_balance <- c(1:timeframe())
+  #   Interest <- c(1:timeframe())
+  #   principal_repayment <- c(1:timeframe())
+  #   cumulative_interest <- c(1:timeframe())
+  #   
+  #   Balance[1] <- as.numeric(input$Loan)
+  #   Interest[1] <- interestRate() * Balance[1]
+  #   principal_repayment[1] <- payment() - Interest[1]
+  #   ending_balance[1] <- Balance[1] - principal_repayment[1]
+  #   cumulative_interest[1] <- Interest[1]
+  #   for (x in c(2:timeframe())){
+  #     
+  #     Balance[x] <-  ending_balance[x - 1]
+  #     Interest[x] <- interestRate() * Balance[x]
+  #     principal_repayment[x] <- payment() - Interest[x]
+  #     ending_balance[x] <- Balance[x] - principal_repayment[x]
+  #     cumulative_interest[x] <- cumulative_interest[x - 1] + Interest[x]
+  # }
+  # })
+  
+  # # Replacing values rest of table
+  #  reactive({ for (x in c(2:timeframe())) {
+  #   Balance[[x]] <- ending_balance()[x - 1]
+  #   Interest[[x]] <- interestRate() * Balance()[x]
+  #   principal_repayment[[x]] <- payment() - Interest()[x]
+  #   ending_balance[[x]] <- Balance()[x] - principal_repayment()[x]
+  #   cumulative_interest[[x]] <- cumulative_interest()[x - 1] + Interest()[x]
+  # }
+  # })
+  
   output$view <- renderTable({
     
-    #Input values from users
-    distType <- input$Distribution
-    inputLoan <- as.numeric(input$Loan)
-    inputInterest <- as.numeric(input$InterestRate)
-    inputYears <- input$Years
-    inputTime <- input$TimeLength
-    
-    #Payment period
-    if (inputTime == "Annually")
-    {period <- 1}
-    if (inputTime == "Semi-Annual")
-    {period <- 2}
-    if (inputTime == "Quarterly")
-    {period <- 4}
-    if (inputTime == "Monthly")
-    {period <- 12}
-    
-    #Initializing list sizes
-    timeframe <- inputYears * period
-    initList <- c(1:timeframe)
-    Year <- initList
-    Balance <- initList
-    ending_balance <- initList
-    Interest <- initList
-    principal_repayment <- initList
-    cumulative_interest <- initList
-    
-    # Interest Rate
-    r <- (inputInterest / 100) / period
-    # Formula for payment
-    Payment <- (inputLoan * r) / (1 - (1 + r) ^ (-period * inputYears))
-    
-    # Initializing the first values in table
-    Balance[1] <- inputLoan
-    Interest[1] <- r * Balance[1]
-    principal_repayment[1] <- Payment - Interest[1]
-    ending_balance[1] <- Balance[1] - principal_repayment[1]
-    cumulative_interest[1] <- Interest[1]
-    
+    # Initializing list sizes
+    Balance <- c(as.numeric(input$Loan), 2:timeframe())
+    Interest <- c(interestRate()*Balance[1], 2:timeframe())
+    principal_repayment <- c(payment() - Interest[1], 2:timeframe())
+    ending_balance <- c(Balance[1] - principal_repayment[1], 2:timeframe())
+    cumulative_interest <- c(Interest[1], 2:timeframe())
+
+
     # Replacing values rest of table
-    for (x in c(2:timeframe)) {
+    for (x in c(2:timeframe())) {
       Balance[x] <- ending_balance[x - 1]
-      Interest[x] <- r * Balance[x]
-      principal_repayment[x] <- Payment - Interest[x]
+      Interest[x] <- interestRate() * Balance[x]
+      principal_repayment[x] <- payment() - Interest[x]
       ending_balance[x] <- Balance[x] - principal_repayment[x]
       cumulative_interest[x] <- cumulative_interest[x - 1] + Interest[x]
     }
-    
-    dataSet <- data.frame(Year,
-                          Balance, 
-                          Payment, 
+
+    dataSet <- data.frame(Year = Year(),
+                          Balance,
+                          Payment = payment(),
                           Interest,
                           "Principal" = principal_repayment,
                           "Ending Balance" = ending_balance,
                           "Total Interest" = cumulative_interest)
     
+    # dataSet <- data.frame(Year = Year(),
+    #                       Balance = f()$Balance, 
+    #                       Payment = payment(), 
+    #                       Interest = f()$Interest,
+    #                       "Principal" = f()$principal_repayment,
+    #                       "Ending Balance" = f()$ending_balance,
+    #                       "Total Interest" = f()$cumulative_interest)
     
-    head(dataSet, n = timeframe)
+    
+    head(dataSet, n = timeframe())
     
   })
   
   
   output$myPlot <- renderPlot({
-    #Input values from users
-    distType <- input$Distribution
-    inputLoan <- as.numeric(input$Loan)
-    inputInterest <- as.numeric(input$InterestRate)
-    inputYears <- input$Years
-    inputTime <- input$TimeLength
     
-    #Payment period
-    if (inputTime == "Annually")
-    {period <- 1}
-    if (inputTime == "Semi-Annual")
-    {period <- 2}
-    if (inputTime == "Quarterly")
-    {period <- 4}
-    if (inputTime == "Monthly")
-    {period <- 12}
+    # Initializing list sizes
+    Balance <- c(as.numeric(input$Loan), 2:timeframe())
+    Interest <- c(interestRate()*Balance[1], 2:timeframe())
+    principal_repayment <- c(payment() - Interest[1], 2:timeframe())
+    ending_balance <- c(Balance[1] - principal_repayment[1], 2:timeframe())
+    cumulative_interest <- c(Interest[1], 2:timeframe())
     
-    #Initializing list sizes
-    timeframe <- inputYears * period
-    initList <- c(1:timeframe)
-    Year <- initList
-    Balance <- initList
-    ending_balance <- initList
-    Interest <- initList
-    principal_repayment <- initList
-    cumulative_interest <- initList
     
-    # Interest Rate
-    r <- (inputInterest / 100) / period
-    # Formula for payment
-    Payment <- (inputLoan * r) / (1 - (1 + r) ^ (-period * inputYears))
-    
-    Balance[1] <- inputLoan
-    Interest[1] <- r * Balance[1]
-    principal_repayment[1] <- Payment - Interest[1]
-    ending_balance[1] <- Balance[1] - principal_repayment[1]
-    cumulative_interest[1] <- Interest[1]
-    
-    for (x in c(2:timeframe)) {
+    # Replacing values rest of table
+    for (x in c(2:timeframe())) {
       Balance[x] <- ending_balance[x - 1]
-      Interest[x] <- r * Balance[x]
-      principal_repayment[x] <- Payment - Interest[x]
+      Interest[x] <- interestRate() * Balance[x]
+      principal_repayment[x] <- payment() - Interest[x]
       ending_balance[x] <- Balance[x] - principal_repayment[x]
-      cumulative_interest[x] <-
-        cumulative_interest[x - 1] + Interest[x]
+      cumulative_interest[x] <- cumulative_interest[x - 1] + Interest[x]
     }
     
-    dataSet<- eventReactive(input$plot,{
-      runif(input$plot)
-    })
-    
-    dataSet <- data.frame(Year,
+    dataSet <- data.frame(Year = Year(),
                           Balance, 
-                          Payment, 
+                          Payment = payment(), 
                           Interest,
-                          "Principal Repayment" = principal_repayment,
+                          "Principal" = principal_repayment,
                           "Ending Balance" = ending_balance,
-                          "Cumulative Interest" = cumulative_interest)
-    
-    # m <- ggplot(dataSet, aes(x = Year, y = cumulative_interest))
-    # m + geom_line(arrow = arrow())
-    # m + geom_line(arrow = arrow(angle = 30, ends = "last", type = "open"))
+                          "Total Interest" = cumulative_interest)
 
-    # g <- ggplot(dataSet, aes(Year,Balance))
-    # g + geom_bar(aes(fill = Year), stat = "identity")
-    
     
     h <- ggplot(dataSet, aes(Year, Balance,
                              width = .9)) + geom_bar(aes(fill = Year), stat =
@@ -145,5 +145,69 @@ shinyServer(function(input, output) {
     )) + guides(fill = FALSE)
     
   })
+  
+  output$downloadData <- downloadHandler(
+    
+    
+    filename = function(){
+      paste('financial.csv', sep='') 
+    },
+    content = function(file) {
+      
+      # Initializing list sizes
+      Balance <- c(as.numeric(input$Loan), 2:timeframe())
+      Interest <- c(interestRate()*Balance[1], 2:timeframe())
+      principal_repayment <- c(payment() - Interest[1], 2:timeframe())
+      ending_balance <- c(Balance[1] - principal_repayment[1], 2:timeframe())
+      cumulative_interest <- c(Interest[1], 2:timeframe())
 
+
+      # Replacing values rest of table
+      for (x in c(2:timeframe())) {
+        Balance[x] <- ending_balance[x - 1]
+        Interest[x] <- interestRate() * Balance[x]
+        principal_repayment[x] <- payment() - Interest[x]
+        ending_balance[x] <- Balance[x] - principal_repayment[x]
+        cumulative_interest[x] <- cumulative_interest[x - 1] + Interest[x]
+      }
+
+      dataSet <- data.frame(Year = Year(),
+                            Balance,
+                            Payment = payment(),
+                            Interest,
+                            "Principal" = principal_repayment,
+                            "Ending Balance" = ending_balance,
+                            "Total Interest" = cumulative_interest)
+      
+      write.csv(dataSet, file)
+    }
+  )
+  
+  output$uploadView <- renderTable({
+    
+    inFile <- input$file1
+    
+    if(is.null(inFile))
+      return(NULL)
+    
+    read.csv(inFile$datapath, sep=input$sep)
+  })
+  
+  output$present_text <- renderText({
+      x <- as.numeric(input$`calc-input`) * (1/ ((1+(input$`calc-interest`)/100)^input$`calc-year`))
+      round(x, digits=2)
+    
+  })
+  
+  output$future_text <- renderText({
+      x <- as.numeric(input$`calc-input`) * ((1+input$`calc-interest`/100)^input$`calc-year`)
+      round(x, digits=2)
+  })
+  
+  output$return_text <- renderText({
+    x <- ((as.numeric(input$`return-input-fv`)/as.numeric(input$`return-input-pv`))^(1/as.numeric(input$`return-year`)))-1
+    x <- x * 100
+    print(paste(round(x, digits=2), "%"))
+    })
+  
 })
--- ui.R
+++ ui.R
@@ -2,10 +2,24 @@ library(shinydashboard)
 
 sidebar <- dashboardSidebar(
   sidebarMenu(
-    menuItem("Loan Calculator", tabName = "loan", icon = icon("calculator")),
-    menuItem("Widgets", tabName = "widgets", icon = icon("th"))
+    menuItem("Loan Calculator", tabName = "loan", icon = icon("bar-chart")),
+    menuItem("Basic Calculations ", tabName = "calculations", icon = icon("calculator")),
+    menuItem("File Upload", tabName = "upload", icon = icon("sign-in"))
+
   )
 )
+
+dropdown <- dropdownMenu(
+  type = "messages",
+  messageItem(
+    from = "System",
+    message = "Welcome User.",
+    icon = icon("smile-o"),
+    time = Sys.Date()
+  )
+)
+
+header <- dashboardHeader(title = "Financial Calculator",dropdown)
 
 body <- dashboardBody(
   tabItems(
@@ -29,38 +43,137 @@ body <- dashboardBody(
                     min=0, max=100, value=20, step=1),
         
         selectInput("TimeLength", "Please Select Timeframe",
-                    choices = c("Annually", "Semi-Annual", "Quarterly", "Monthly"))
+                    choices = c("Annually", "Semi-Annual", "Quarterly", "Monthly")),
+        downloadButton('downloadData', 'Download')
       ),
+      
       box(
         width = 9,
-        tabsetPanel(id = "tabset",
-                    tabPanel("TableView", (tableOutput("view"))),
-                    tabPanel("PlotView", (plotOutput("myPlot")))
+        solidHeader = TRUE,
+        status = "primary",
+        tabBox(
+          title = tagList(shiny::icon("area-chart"), "Loan"),
+          width = 12,
+          
+          tabPanel("TableView", (tableOutput("view"))),
+          tabPanel("PlotView", (plotOutput("myPlot")))
+        )
       )
-      )
-      )),
+      
+      )), 
     
-    tabItem(tabName = "widgets",
+    tabItem(
+      tabName = "calculations",
+      box(
+        width  = 12,
+        solidHeader = TRUE,
+        status = "primary",
+            tabBox(
+              title = tagList(shiny::icon("balance-scale"), "Calculations"),
+              width = 12,
+              tabPanel("PV/FV Calculator",
+                       fluidRow(
+                         box(
+                           width = 4,
+                           title = "Calculator",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary",
+                  
+                           textInput("calc-input", "Amount", 1000),
+                  
+                           sliderInput("calc-interest", "Please Select the Interest Rate: ",
+                            min=0, max=20, value=5, step=1),
+                           
+                           sliderInput("calc-year", "Please Select the Number of Years: ",
+                            min = 0, max = 100, value = 5, step = 1)
+                           
+                         ),
+                         box(
+                           width = 4,
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "warning",
+                           title = "Present Value",
+                           verbatimTextOutput("present_text")
+                         ),
+                         box(
+                           width = 4,
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "danger",
+                           title = "Future Value",
+                           verbatimTextOutput("future_text")
+                         )
+                       )
+              ),
+              tabPanel("Return",
+                       fluidRow(
+                         box(
+                           width = 4,
+                           title = "Calculator",
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "primary",
+                           
+                           textInput("return-input-pv", "Present Value: ", 1000),
+                           textInput("return-input-fv", "Future Value: ", 1200),
+                           textInput("return-year", "Please Select the Number of Years: ", 5)
+                           
+                         ),
+                         box(
+                           width = 4,
+                           solidHeader = TRUE,
+                           collapsible = TRUE,
+                           status = "info",
+                           title = "Rate of Return",
+                           verbatimTextOutput("return_text")
+                         )
+                       )
+              )
+            )
+    )
+    ), 
+    
+    tabItem(tabName = "upload",
             
             fluidRow(
               box(
-                title = "Work In Progress",
-                width = 12,
+                title = "Document Upload",
+                width = 3,
                 solidHeader = TRUE,
                 collapsible = TRUE,
                 status = "primary",
                 
-                selectInput("Distribution", "Please Select Calculation Type",
-                        choices = c("Present Value", "Future value"))
-            )
-            )
-     )
+                fileInput('file1', 'Choose CSV File',
+                          accept=c('text/csv', 
+                                   'text/comma-separated-values,text/plain', 
+                                   '.csv')),
+                tags$hr(),
+                radioButtons('sep', 'Separator',
+                             c(Comma=',',
+                               Semicolon=';',
+                               Tab='\t'),
+                             ',')
+              ),
+              box(
+                title = "Data Statistics",
+                solidHeader = TRUE,
+                collapsible = TRUE,
+                status = "success",
+                width = 9,
+                tableOutput('uploadView')
+              )
+    )
+  )
+  
 ))
+
 
 shinyUI(fluidPage(
   
   dashboardPage(
-    dashboardHeader(title = "Financial Calculator"),
+    header,
     sidebar,
     body
   )
